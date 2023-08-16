3feeb41a336a66f49bda5b1b6304bde1dc0ad831
k1nnyyY <osman.haciev231@gmail.com>
2023-08-16 14:53:55 +0400

    ff

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,28 @@
 // App.jsx
-import { useQuery } from '@apollo/client';
-import { USER_PREVIEW_QUERY } from './query/queries';
-import ChatPage from './pages/chatPage';
-import Logo from './assets/logo.png';
+import { useQuery } from "@apollo/client";
+import { USER_PREVIEW_QUERY } from "./query/queries";
+import ChatPage from "./pages/chatPage";
+import Logo from "./assets/logo.png";
 function App() {
-  // const { loading, error, data } = useQuery(USER_PREVIEW_QUERY, {
-  //   variables: { asVIP: false }
-  // });
-
-  // if (loading) return <p>Loading...</p>;
-  // if (error) return <p>Error: {error.message}</p>;
-
-  // const user = data.userPreview;
-
+  const { loading, error, data } = useQuery(USER_PREVIEW_QUERY, {
+    variables: { asVIP: false },
+  });
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+  const user = data.userPreview;
+  console.log(user);
   return (
     <>
-      <ChatPage/>
-      {/* <div>
+      <ChatPage />
+      <div>
         <img src={user.avatar.path} alt="User Avatar" />
       </div>
-      <h1>{user.firstname} {user.lastname}</h1>
+      <h1>
+        {user.firstname} {user.lastname}
+      </h1>
       <p>ID: {user.id}</p>
       <p>Phone: {user.phoneNumber}</p>
       <p>Status: {user.status.value}</p>
-      ... остальная часть вашего кода ... */}
     </>
   );
 }
--- src/apolloClient.js
+++ src/apolloClient.js
@@ -1,11 +1,28 @@
-import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
+// client.js (или название вашего файла)
+
+import { ApolloClient, InMemoryCache, HttpLink, ApolloLink } from '@apollo/client';
+
+// Middleware для добавления заголовка авторизации
+const authLink = new ApolloLink((operation, forward) => {
+  // Используйте заголовок авторизации с вашим токеном
+  operation.setContext({
+    headers: {
+      Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjkwNTQwMzA2LCJleHAiOjE2OTgzMTYzMDZ9.mTTzRDlPwrYXMzBxM-NHbA4uJP3TBHfxscB_D4ZzW3g`
+    }
+  });
+
+  return forward(operation);
+});
 
 const httpLink = new HttpLink({
   uri: 'https://korpustage.ru/search'
 });
 
+// Объединение authLink и httpLink
+const link = authLink.concat(httpLink);
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: link,
   cache: new InMemoryCache()
 });
 
